6378adc237d227ab28f4d92fe23a7fa98aa73013
duracellB <i.schlae.h@gmail.com>
2023-07-27 18:58:48 -0400

    added timer functionality

--- src/pages/Exam.jsx
+++ src/pages/Exam.jsx
@@ -1,3 +1,28 @@
-export default function Exam() {
-  return <h1>Exam</h1>;
-}
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Timer from "../components/Timer";
+
+const Exam = () => {
+  const location = useLocation();
+  const { totalSeconds } = location.state;
+  const [isTimerDone, setIsTimerDone] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => { //listening for timer completion
+    if (isTimerDone) { //the timer ended
+      navigate("/results"); //redirect to results page
+    }
+  }, [isTimerDone, navigate]);
+
+  return (
+    <>
+      <h1>Exam</h1>
+      <Timer
+        totalSeconds={totalSeconds}
+        onTimerDone={() => setIsTimerDone(true)}
+      />
+    </>
+  );
+};
+
+export default Exam;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,3 +1,41 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Landing() {
-  return <h1>Landing</h1>;
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const navigate = useNavigate();
+
+  const handleStartTimer = () => {
+    const totalSeconds = hours * 3600 + minutes * 60; //convert hours + minutes to seconds
+    navigate("/exam", { state: { totalSeconds } }); //redirect to the exam page, with the seconds as a state
+  };
+
+  return (
+    <>
+      <h1>Landing</h1>
+      <div>
+        <h1>Set the timer</h1>
+        <div>
+          <label>
+            Hours:
+            <input
+              type="number"
+              value={hours}
+              onChange={e => setHours(e.target.value)}
+            />
+          </label>
+          <label>
+            Minutes:
+            <input
+              type="number"
+              value={minutes}
+              onChange={e => setMinutes(e.target.value)}
+            />
+          </label>
+        </div>
+        <button onClick={handleStartTimer}>Start Timer</button>
+      </div>
+    </>
+  );
 }
