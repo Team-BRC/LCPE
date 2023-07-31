c1b0fa204ee50791369ed37f43ad27b993a55431
duracellB <i.schlae.h@gmail.com>
2023-07-31 15:36:25 -0400

    added exit exam + confirmation

---
+++ src/components/ExitButton.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Header, Icon, Modal } from "semantic-ui-react";
+
+const formatTime = timeInSeconds => {
+  //time seconds -> string
+  const hours = Math.floor(timeInSeconds / 3600);
+  const minutes = Math.floor((timeInSeconds % 3600) / 60);
+  const seconds = timeInSeconds % 60;
+
+  return `${String(hours).padStart(2, "0")} hours, ${String(minutes).padStart(
+    2,
+    "0"
+  )} minutes, and ${String(seconds).padStart(2, "0")} seconds`;
+};
+
+function ExitButton({ remainingTime, totalSeconds }) {
+  const [open, setOpen] = React.useState(false);
+  const navigate = useNavigate();
+
+  return (
+    <Modal
+      basic
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      size="small"
+      trigger={<Button>End Exam</Button>}
+    >
+      <Header icon>
+        <Icon name="archive" />
+        End Exam
+      </Header>
+      <Modal.Content>
+        {totalSeconds > 0 ? (
+          <p>
+            You still have {formatTime(remainingTime)} left, are you sure you
+            want to end your exam?
+          </p>
+        ) : (
+          <p>
+            Once you end your exam, you cannot change your answers. Are you sure
+            you want to end?
+          </p>
+        )}
+      </Modal.Content>
+      <Modal.Actions>
+        <Button basic color="red" inverted onClick={() => navigate("/results")}>
+          <Icon name="remove" /> Finish and View Results
+        </Button>
+        <Button color="green" inverted onClick={() => setOpen(false)}>
+          <Icon name="checkmark" /> Continue Exam
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+}
+
+export default ExitButton;
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,36 +1,44 @@
 import React, { useEffect, useState } from "react";
 
-const Timer = ({ totalSeconds, onTimerDone }) => {
+const formatTime = timeInSeconds => {
+  //time seconds -> string
+  const hours = Math.floor(timeInSeconds / 3600);
+  const minutes = Math.floor((timeInSeconds % 3600) / 60);
+  const seconds = timeInSeconds % 60;
+
+  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+    2,
+    "0"
+  )}:${String(seconds).padStart(2, "0")}`;
+};
+
+const Timer = ({
+  totalSeconds,
+  remainingTime,
+  setRemainingTime,
+  onTimerDone,
+}) => {
+  // if (totalSeconds > 0) {
+  //only do this if there is a set timer
+  useEffect(() => {
+    //monitors when the timer ends
+    if (totalSeconds > 0 && remainingTime > 0) {
+      //if there is a timer & there is time left
+      const timer = setInterval(() => {
+        setRemainingTime(prev => prev - 1); // decriment the timer
+      }, 1000);
+      return () => clearInterval(timer); //getting rid of space
+    } else if (remainingTime <= 0) {
+      //if the timer ran out
+      // the time ran out
+      onTimerDone(); //function that changes the state, which will in turn dredirect
+    }
+  }, [totalSeconds, remainingTime, setRemainingTime, onTimerDone]);
+
   if (totalSeconds > 0) {
-    const [remainingTime, setRemainingTime] = useState(totalSeconds); //the starting time state
-
-    useEffect(() => {
-      //monitors when the timer ends
-      if (remainingTime > 0) {
-        const timer = setInterval(() => {
-          setRemainingTime(prev => prev - 1); // decriment the timer
-        }, 1000);
-        return () => clearInterval(timer); //getting rid of space
-      } else {
-        // the time ran out
-        onTimerDone(); //function that changes the state, which will in turn dredirect
-      }
-    }, [remainingTime, onTimerDone]);
-
-    const formatTime = timeInSeconds => {
-      //time seconds -> string
-      const hours = Math.floor(timeInSeconds / 3600);
-      const minutes = Math.floor((timeInSeconds % 3600) / 60);
-      const seconds = timeInSeconds % 60;
-
-      return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
-        2,
-        "0"
-      )}:${String(seconds).padStart(2, "0")}`;
-    };
-
     return <div>{formatTime(remainingTime)}</div>;
   }
+  // }
 };
 
 export default Timer;
