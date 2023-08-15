c7d8dd10b7499870a493ec28e9275d75b7e38e8a
Mathew <matsantos114@gmail.com>
2023-08-15 12:52:12 -0400

    build 2.3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function fetchData() {
   // Load environment variables from .env file
-  const response = await fetch(`https://brc-lactation-consultant-practice-exam.onrender.com/api`);
+  const response = await fetch(`/api`);
   const data = await response.json();
   console.log(data);
   return data;
