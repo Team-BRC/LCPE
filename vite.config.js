764818e61d8b5b8d4db805b8a6e2ce82f0360dd9
Mathew <matsantos114@gmail.com>
2023-09-05 13:10:11 -0400

    redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-/* eslint-disable */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   build: {
     outDir: path.join(__dirname, "..", "public"),
     emptyOutDir: true,
