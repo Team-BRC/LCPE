e1a93ff151b0d47c2b66499f6cb3f5352a6a72cc
Mathew <matsantos114@gmail.com>
2023-09-06 09:58:49 -0400

    deploying site

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/LCPE/',
-  build: {
-    outDir: path.join(__dirname, "..", "public"),
-    emptyOutDir: true,
-  },
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://0.0.0.0:3000",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    },
-  },
-  serve: {
-    static: ["./src"],
-    mimeTypes: {
-      "text/jsx": "application/javascript",
-    },
-  },
+  base: "/LCPE/",
 });
