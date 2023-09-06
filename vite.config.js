a64a7a80b497bd9450cdd83b1498e8e8e1621a69
Mathew <matsantos114@gmail.com>
2023-09-06 11:29:21 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/LCPE/",
-  server: {
-    mimeTypes: {
-      "application/javascript": [".js", ".jsx"],
-    },
-  },
+  base: "/",
 });
